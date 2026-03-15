cb789b5f2f368e8c9582ea07b5aa471cdbe41622
Alex Cutforth <alex@densem.net>
2026-03-15 18:31:05 +1300

    feat: circle intersection

--- linalg/src/line.rs
+++ linalg/src/line.rs
@@ -3,7 +3,8 @@ use core::{
     ops::{AddAssign, Div, Mul, Neg, Sub, SubAssign},
 };
 
-use num_traits::{One, Zero};
+use num_traits::{ConstOne, ConstZero, One, Zero};
+use uom::si::f32::{Length, Ratio};
 
 use crate::{
     matrix::Matrix,
@@ -33,26 +34,30 @@ impl<const N: usize, T: Copy> Line<N, T> {
         start + end * t - start * t
     }
 
-    pub fn closest_point<S: Copy, O: Copy>(self, point: Vector<N, T>) -> Vector<N, T>
+    pub fn clamped_lerp<S: Copy>(self, t: S) -> Vector<N, T>
     where
-        T: Mul<T, Output = O> + Mul<S, Output = T> + AddAssign + SubAssign,
+        T: Mul<S, Output = T> + AddAssign + SubAssign,
         S: Zero + One + PartialOrd,
-        O: Div<O, Output = S> + Sum,
     {
-        let Self(start, end) = self;
-
-        let v = end - start;
-        let d = point - start;
-
-        let t = v.dot(d) / v.dot(v);
-
         if t <= S::zero() {
-            return start;
+            self.lerp(S::zero())
         } else if t >= S::one() {
-            return end;
+            self.lerp(S::one())
+        } else {
+            self.lerp(t)
         }
+    }
 
-        start + v * t
+    pub fn bounded_lerp<S: Copy>(self, t: S) -> Option<Vector<N, T>>
+    where
+        T: Mul<S, Output = T> + AddAssign + SubAssign,
+        S: Zero + One + PartialOrd,
+    {
+        if t >= S::zero() && t <= S::one() {
+            Some(self.lerp(t))
+        } else {
+            None
+        }
     }
 
     pub fn n_intersection<S: Copy>(self, other: Self) -> Option<Vector<N, T>>
@@ -125,9 +130,97 @@ impl<T: Copy> Line<2, T> {
     }
 }
 
+pub enum LineCircleIntersection<const N: usize, T> {
+    None,
+    Single(Vector<N, T>),
+    Dual(Vector<N, T>, Vector<N, T>),
+}
+
+impl<const N: usize> Line<N, Length> {
+    pub fn closest_point(self, point: Vector<N, Length>) -> Vector<N, Length> {
+        let Self(start, end) = self;
+
+        let v = end - start;
+        let d = point - start;
+
+        let t = v.dot(d) / v.dot(v);
+
+        self.clamped_lerp(t)
+    }
+
+    pub fn circle_intersection_scalars(
+        self,
+        center: Vector<N, Length>,
+        radius: Length,
+    ) -> Option<(Ratio, Ratio)> {
+        let Self(start, end) = self;
+
+        let v = end - start;
+        let d = start - center;
+
+        let a = v.dot(v);
+        let b = 2.0 * d.dot(v);
+        let c = d.dot(d) - radius * radius;
+
+        let discriminant = b * b - (4.0 * a * c);
+
+        if discriminant.is_sign_negative() {
+            return None;
+        }
+
+        Some((
+            (discriminant.sqrt() - b) / (2.0 * a),
+            (-discriminant.sqrt() - b) / (2.0 * a),
+        ))
+    }
+
+    pub fn circle_intersection(
+        self,
+        center: Vector<N, Length>,
+        radius: Length,
+    ) -> LineCircleIntersection<N, Length> {
+        let t = self.circle_intersection_scalars(center, radius);
+
+        match t {
+            Some((t1, t2)) => {
+                let p1 = self.bounded_lerp(t1);
+                let p2 = self.bounded_lerp(t2);
+
+                match (p1, p2) {
+                    (Some(p1), Some(p2)) => {
+                        if t1 == t2 {
+                            LineCircleIntersection::Single(p1)
+                        } else {
+                            LineCircleIntersection::Dual(p1, p2)
+                        }
+                    }
+                    (Some(p), None) | (None, Some(p)) => LineCircleIntersection::Single(p),
+                    (None, None) => LineCircleIntersection::None,
+                }
+            }
+            None => LineCircleIntersection::None,
+        }
+    }
+
+    pub fn circle_intersection_snapped(
+        self,
+        center: Vector<N, Length>,
+        radius: Length,
+    ) -> Vector<N, Length> {
+        let t = self.circle_intersection_scalars(center, radius);
+
+        let Some((t, _)) = t else {
+            return self.closest_point(center);
+        };
+
+        self.clamped_lerp(t)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use approx::assert_relative_eq;
+    use uom::si::{f32::Length, length::meter};
 
     use crate::{line::Line, real_vector};
 
@@ -205,5 +298,25 @@ mod test {
         );
 
         assert_eq!(line_seg1.intersection(line_seg2), None)
+    }
+
+    #[test]
+    fn test_circle_intersection_end() {
+        let start = real_vector!(Length::meter, 0.0, 0.0);
+        let end = real_vector!(Length::meter, 0.0, 1.0);
+        let line_seg1 = Line(start, end);
+
+        let position = real_vector!(Length::meter, -2.0, -2.0);
+
+        let length = Length::new::<meter>(1.5);
+
+        assert_eq!(
+            line_seg1.circle_intersection_scalars(position, length),
+            None
+        );
+        assert_eq!(
+            line_seg1.circle_intersection_snapped(position, length),
+            start
+        );
     }
 }
